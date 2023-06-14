c1c01a844c088e0c0a42db4ac0905f4428d2f754
KimDowi <rlaehdnl@gmail.com>
2023-06-14 21:52:02 +0900

    Changes

--- pythonProject/시저 암호.py
+++ pythonProject/시저 암호.py
@@ -3,7 +3,7 @@ def solution(s, n):
     s = list(s)
     for i in range(len(s)) :
         if s[i] == ' ' : continue
-        corr = ord('A') if s[i].isupper() else ord('a') # 예) A > B ? a : b
-        s[i] = chr((ord(s[i]) - corr + n) % 26 + corr)
+        corr = ord('A') if s[i].isupper() else ord('a') # ord('A') == 65 ord('a') == 97
+        s[i] = chr((ord(s[i]) - corr + n) % 26 + corr) # 알파벳 총 갯수 26개 (대소문자 구분x)
     answer = ''.join(s)
     return answer
---
+++ pythonProject/튜플.py
@@ -0,0 +1,21 @@
+def solution(s):
+    data = s[2:-2].split("},{")
+    data = sorted(data, key=lambda x:len(x)) # 길이별 오름차순 정렬. lambda <표현식> : <표현식을 이용한 연산>
+    answer = []
+    for item in data :
+        item = list(map(int,item.split(",")))
+        for value in item :
+            if value not in answer :
+                answer.append(value)
+    return answer
+
+def solution2(s):
+    answer = {} # 딕셔너리 사용
+    s = sorted(s[2:-2].split("},{"), key=len)
+    for tuples in s :
+        elements = tuples.split(',')
+        for element in elements :
+            number = int(element)
+            if number not in answer :
+                answer[number] = 1 # 딕셔너리 키 사용
+    return list(answer) # 딕셔너리 값이 배정된 키만 배열로 리턴됨
