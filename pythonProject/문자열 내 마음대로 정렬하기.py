7921c3da358d5a7d9f8bdc62e9b5158f0358afbd
KimDowi <rlaehdnl@gmail.com>
2023-06-29 22:25:01 +0900

    Changes

--- pythonProject/문자열 내 마음대로 정렬하기.py
+++ pythonProject/문자열 내 마음대로 정렬하기.py
@@ -1,8 +1,8 @@
-def solution(strings, n):
+def solution(strings, n): # 특정 단어 순으로 정렬
     return sorted(sorted(strings), key=lambda x: x[n])
 
-def solution2(strings, n):
+def solution2(strings, n): # 합쳐서 정렬
     return sorted(strings, key=lambda x: x[n] + x)
 
-def solution3(strings, n):
+def solution3(strings, n): #튜플 형식 정렬
     return sorted(strings, key=lambda x: (x[n] , x))
